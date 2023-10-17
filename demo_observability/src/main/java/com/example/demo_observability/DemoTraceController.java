6154119a36cce35f946a43797acc5a9bbf53463c
up1 <somkiat.p@gmail.com>
2023-10-17 23:30:00 +0700

    Add log

--- demo_observability/src/main/java/com/example/demo_observability/DemoTraceController.java
+++ demo_observability/src/main/java/com/example/demo_observability/DemoTraceController.java
@@ -1,11 +1,14 @@
 package com.example.demo_observability;
 
 import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoTraceController {
+    private static final Logger log = LoggerFactory.getLogger(DemoTraceController.class);
 
     private final DemoService demoService;
 
@@ -16,6 +19,7 @@ public class DemoTraceController {
     @GetMapping("hi")
     @Observed(name = "do.sth", contextualName = "say-hi")
     public String sayHi() {
+        log.info("Got a request of sayHi");
         return demoService.doSth("123");
     }
 

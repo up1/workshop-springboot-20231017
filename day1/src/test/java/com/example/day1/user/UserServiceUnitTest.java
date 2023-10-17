17e9a6c5ba5c7ce0aeec9ce8d5907a0a07cc7f6a
up1 <somkiat.p@gmail.com>
2023-10-17 16:56:52 +0700

    Add fail case for service layer

--- day1/src/test/java/com/example/day1/user/UserServiceUnitTest.java
+++ day1/src/test/java/com/example/day1/user/UserServiceUnitTest.java
@@ -29,4 +29,16 @@ class UserServiceUnitTest {
         assertNotNull(result);
         assertEquals("Mock", result.getName());
     }
+
+    @Test
+    void case02() {
+        // Arrange
+        when(userRepository.findById(3))
+                .thenReturn(Optional.empty());
+        UserService service = new UserService(userRepository);
+        // Act and assert
+        Exception exception = assertThrows(UserNotFoundException.class, () ->
+                service.get(3));
+        assertEquals("User id=3 not found", exception.getMessage());
+    }
 }

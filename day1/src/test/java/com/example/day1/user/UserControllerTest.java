755f5f29fb4134d4cd272e5495900ac28d69eff1
up1 <somkiat.p@gmail.com>
2023-10-17 13:47:39 +0700

    Test for error

--- day1/src/test/java/com/example/day1/user/UserControllerTest.java
+++ day1/src/test/java/com/example/day1/user/UserControllerTest.java
@@ -17,12 +17,23 @@ class UserControllerTest {
 
     @Test
     @DisplayName("Success for get user by id = 1")
-    void getUserByID() {
+    void case01() {
         // Act
         UserResponse response = testRestTemplate.getForObject(
                 "/api/v1/user/1", UserResponse.class);
         // Assert
         assertEquals(1, response.getId());
         assertEquals("Somkiat", response.getName());
+    }
+
+    @Test
+    @DisplayName("Invalid input")
+    void case02() {
+        // Act
+        MyErrorResponse response = testRestTemplate.getForObject(
+                "/api/v1/user/2", MyErrorResponse.class);
+        // Assert
+        assertEquals(10000, response.getCode());
+        assertEquals("Input invalid", response.getDescription());
     }
 }

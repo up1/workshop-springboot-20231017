c90e8cf7137caba0e89186a3df84a088bff4adb2
up1 <somkiat.p@gmail.com>
2023-10-17 11:38:17 +0700

    Create controller

--- day1/src/main/java/com/example/day1/user/UserResponse.java
+++ day1/src/main/java/com/example/day1/user/UserResponse.java
@@ -2,9 +2,11 @@ package com.example.day1.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse{
 	private int id;
 	private String name;

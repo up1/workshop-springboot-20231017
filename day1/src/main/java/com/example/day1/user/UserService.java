42ab7cb1cb3eb04296c5d838c4816ede149708bc
up1 <somkiat.p@gmail.com>
2023-10-17 15:19:10 +0700

    Integrate repository to service layer

--- day1/src/main/java/com/example/day1/user/UserService.java
+++ day1/src/main/java/com/example/day1/user/UserService.java
@@ -1,13 +1,24 @@
 package com.example.day1.user;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
     public UserResponse get(int id) {
-        return new UserResponse(1, "Somkiat");
+//        return new UserResponse(1, "Somkiat");
+        Optional<MyUser> result = userRepository.findById(id);
+        if(result.isPresent()) {
+            return new UserResponse(result.get().getId(),
+                    result.get().getName());
+        }
+        throw new RuntimeException("User not found");
     }
 
 }

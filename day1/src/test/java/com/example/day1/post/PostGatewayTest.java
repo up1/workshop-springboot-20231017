8df829808f10453e355a04f08c7b9f17c70f6ae6
up1 <somkiat.p@gmail.com>
2023-10-18 15:06:06 +0700

    Add gateway testing

---
+++ day1/src/test/java/com/example/day1/post/PostGatewayTest.java
@@ -0,0 +1,25 @@
+package com.example.day1.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostGatewayTest {
+
+    @Autowired
+    private PostGateway postGateway;
+
+    @Test
+    void getDataById() {
+        // Act
+        Optional<PostResponse> response = postGateway.getDataById(1);
+        // Assert
+        assertTrue(response.isPresent());
+        assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", response.get().getTitle());
+    }
+}

4a440b1c4c2f3fee2f52251a49f13248a1d6ccff
up1 <somkiat.p@gmail.com>
2023-10-18 16:10:12 +0700

    Using wiremock stub runner

--- day1/src/test/java/com/example/day1/post/PostGatewayTest.java
+++ day1/src/test/java/com/example/day1/post/PostGatewayTest.java
@@ -3,23 +3,47 @@ package com.example.day1.post;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 8882)
 class PostGatewayTest {
 
     @Autowired
     private PostGateway postGateway;
 
     @Test
-    void getDataById() {
+    void getDataById() throws IOException {
+        // Arrange
+        stubFor(get(urlPathEqualTo("/posts/1"))
+                .willReturn(aResponse()
+                        .withBody(read("classpath:postApiResponse.json"))
+                        .withHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withStatus(200)));
         // Act
         Optional<PostResponse> response = postGateway.getDataById(1);
         // Assert
         assertTrue(response.isPresent());
         assertEquals("Mock title", response.get().getTitle());
+    }
+
+    public static String read(String filePath) throws IOException {
+        File file = ResourceUtils.getFile(filePath);
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }

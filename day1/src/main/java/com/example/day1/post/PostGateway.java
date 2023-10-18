3150d5392457af0ebda4fe20b7f1e0c05720b280
up1 <somkiat.p@gmail.com>
2023-10-18 15:48:16 +0700

    Read from application.properties

--- day1/src/main/java/com/example/day1/post/PostGateway.java
+++ day1/src/main/java/com/example/day1/post/PostGateway.java
@@ -1,19 +1,26 @@
 package com.example.day1.post;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 @Component
-@AllArgsConstructor
 public class PostGateway {
 
     private final RestTemplate restTemplate;
 
+    @Value("${post.url}")
+    private final String host;
+
+    public PostGateway(RestTemplate restTemplate, String host) {
+        this.restTemplate = restTemplate;
+        this.host = host;
+    }
+
     public Optional<PostResponse> getDataById(int id) {
-        String url = "https://jsonplaceholder.cypress.io/posts/" + id;
+        String url = host + "/posts/" + id;
         try {
             PostResponse response = restTemplate.getForObject(url, PostResponse.class);
             return Optional.ofNullable(response);

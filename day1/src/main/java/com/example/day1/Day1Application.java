34bed2ea9f5b10e330de064ae8f000a3ba446d3a
up1 <somkiat.p@gmail.com>
2023-10-18 10:43:54 +0700

    Get number of beans

--- day1/src/main/java/com/example/day1/Day1Application.java
+++ day1/src/main/java/com/example/day1/Day1Application.java
@@ -2,12 +2,19 @@ package com.example.day1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Day1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day1Application.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(Day1Application.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+        Arrays.stream(beans).forEach(System.out::println);
+		System.out.println("=>>" + context.getBeanDefinitionCount());
 	}
 
 }
---
+++ day1/src/main/java/com/example/day1/demo/Hello.java
@@ -0,0 +1,7 @@
+package com.example.day1.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+}
